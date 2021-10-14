ff5420b03b691acdb9553ddae4215faecda7c896
Omar Al-Ithawi <i@omardo.com>
2021-10-14 11:34:54 +0300

    add cache

--- site_config_client/openedx/settings/common.py
+++ site_config_client/openedx/settings/common.py
@@ -3,17 +3,20 @@
 
 from site_config_client import Client
 from site_config_client.google_cloud_storage import GoogleCloudStorage
-# from site_config_client.django_cache import DjangoCache
+from site_config_client.django_cache import DjangoCache
 
 
 def plugin_settings(settings):
     read_only_storage = GoogleCloudStorage(settings.SITE_CONFIG_READ_ONLY_BUCKET)
-    # cache = DjangoCache(timeout=settings.SITE_CONFIG_CAHCE_TIMEOUT)
+    cache = DjangoCache(
+        cache_name=getattr(settings, 'SITE_CONFIG_CAHCE_NAME', 'default'),
+        default_timeout=getattr(settings, 'SITE_CONFIG_CAHCE_TIMEOUT', None),
+    )
     settings.SITE_CONFIG_CLIENT = Client(
         base_url=settings.SITE_CONFIG_BASE_URL,
         api_token=settings.SITE_CONFIG_API_TOKEN,
         read_only_storage=read_only_storage,
-        # cache=cache,
+        cache=cache,
     )
 
 
