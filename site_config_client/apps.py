b6d6102356e611924f8b0635cb0660d12692f1ca
Omar Al-Ithawi <i@omardo.com>
2021-10-21 15:35:57 +0300

    refactor: don't require Open edX for Django apps
    Address anders comment: https://github.com/appsembler/site-configuration-client/pull/7/files#r733610196

--- site_config_client/apps.py
+++ site_config_client/apps.py
@@ -1,23 +1,33 @@
 from django.apps import AppConfig
-from openedx.core.djangoapps.plugins.constants import (
-    PluginSettings, ProjectType, SettingsType)
+
 
 
 class SiteConfigApp(AppConfig):
     """
-    Provides edX configuration for Site Configuration
+    Django app configs.
     """
     name = 'site_config_client'
     label = 'site_config_client'
     verbose_name = 'Site configuration API client and Open edX plugin.'
 
-    plugin_app = {
-        PluginSettings.CONFIG: {
-            ProjectType.LMS: {
-                SettingsType.PRODUCTION: {
-                    PluginSettings.RELATIVE_PATH: 'settings.production'},
-                SettingsType.COMMON: {
-                    PluginSettings.RELATIVE_PATH: 'settings.common'},
-            }
-        },
-    }
+
+class OpenedXSiteConfigApp(SiteConfigApp):
+    """
+    Open edX app configs.
+    """
+
+    @property
+    def plugin_app(self):
+        from openedx.core.djangoapps.plugins.constants import (
+    PluginSettings, ProjectType, SettingsType)
+
+        return {
+            PluginSettings.CONFIG: {
+                ProjectType.LMS: {
+                    SettingsType.PRODUCTION: {
+                        PluginSettings.RELATIVE_PATH: 'settings.production'},
+                    SettingsType.COMMON: {
+                        PluginSettings.RELATIVE_PATH: 'settings.common'},
+                }
+            },
+        }
