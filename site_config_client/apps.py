51d6ae883e5a42f5adc81e1989109b33d654571b
Omar Al-Ithawi <i@omardo.com>
2021-10-21 15:39:43 +0300

    refactor: delay import, but keep in one class

--- site_config_client/apps.py
+++ site_config_client/apps.py
@@ -4,22 +4,25 @@ from django.apps import AppConfig
 
 class SiteConfigApp(AppConfig):
     """
-    Django app configs.
+    Django and Open edX app configs.
     """
     name = 'site_config_client'
     label = 'site_config_client'
     verbose_name = 'Site configuration API client and Open edX plugin.'
 
-
-class OpenedXSiteConfigApp(SiteConfigApp):
-    """
-    Open edX app configs.
-    """
-
     @property
     def plugin_app(self):
+        """
+        Open edX-specific configurations.
+
+        This is not used by Django.
+        """
+        # Import locally to allow non-Open edX use.
         from openedx.core.djangoapps.plugins.constants import (
-    PluginSettings, ProjectType, SettingsType)
+            PluginSettings,
+            ProjectType,
+            SettingsType,
+        )
 
         return {
             PluginSettings.CONFIG: {
