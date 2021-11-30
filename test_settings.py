c8fbc294cafedf2000aa6e8fd03057e48c65d785
Omar Al-Ithawi <i@omardo.com>
2021-11-30 18:16:59 +0300

    add feature flag for gradual migration
    add `SiteConfigClientEnabled` model to allow gradual migration from deprecated site configuration

--- test_settings.py
+++ test_settings.py
@@ -5,6 +5,9 @@ Empty pytest settings.
 SECRET_KEY = 'dummy secret key'  # nosec
 
 INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.admin',
     'site_config_client',
 ]
 
