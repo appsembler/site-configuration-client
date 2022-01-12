185df87ac8d3f0bfe5c0da68e9f0451a50ef3805
Omar Al-Ithawi <i@omardo.com>
2022-01-12 14:07:32 +0300

    fix errors in test settings
    otheriwse makemigrations won't work and raise warnings
    
    ```
    $ tox -e shell-django -- python manage.py makemigrations site_config_client
    ```

--- test_settings.py
+++ test_settings.py
@@ -8,10 +8,34 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.admin',
+    'django.contrib.messages',
     'site_config_client',
 ]
 
 FEATURES = {}
+
+MIDDLEWARE = [
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 DATABASES = {
     'default': {
