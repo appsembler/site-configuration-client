5d76439550299f6b7aa4676d76c0baf5f106f54b
Omar Al-Ithawi <i@omardo.com>
2022-05-19 12:10:40 +0300

    bump to 0.1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='site-configuration-client',
-    version='0.1.6',
+    version='0.1.7',
     description='Python client library for Site Configuration API',
     long_description=read('README.rst'),
     classifiers=[
